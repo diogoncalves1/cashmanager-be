0a3e9591e6fe35ca03316c6ce6fdac4a6ddfd9df
diogogoncalves <dmgdark95@gmail.com>
2026-02-16 22:02:43 +0000

    fix(user preferences): Update table, lang to fk

--- Modules/UserPreferences/Database/Migrations/2025_10_28_102305_create_user_preferences_table.php
+++ Modules/UserPreferences/Database/Migrations/2025_10_28_102305_create_user_preferences_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('user_preferences', function (Blueprint $table) {
             $table->unsignedBigInteger('user_id')->unique();
             $table->unsignedBigInteger('currency_id')->nullable();
-            $table->string('lang', 191)->default('en');
+            $table->string('lang', 191)->default('pt')->nullable();
 
             $table->timestamps();
 
